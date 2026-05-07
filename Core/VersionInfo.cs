4f864fbbf2d902325877470c0eaab83a9bff3121
MC J <jmc8@qq.com>
2026-05-08 01:07:49 +0800

    添加项目文件。

---
+++ Core/VersionInfo.cs
@@ -0,0 +1,9 @@
+namespace BetterSaveSlots.Core;
+
+public static class VersionInfo
+{
+    public const string Name = "BetterSaveSlots";
+    public const string Version = "1.0.2";
+
+    public static string Tag => $"[{Name} v{Version}]";
+}
