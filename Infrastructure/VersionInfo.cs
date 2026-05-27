7b129cdb200eccce80e18f5dc387e6f5edf95486
MC J <jmc8@qq.com>
2026-05-27 18:22:34 +0800

    改用JML本地化包装

--- Infrastructure/VersionInfo.cs
+++ Infrastructure/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace BetterSaveSlots.Infrastructure;
 public static class VersionInfo
 {
     public const string Name = "BetterSaveSlots";
-    public const string Version = "0.1.6";
+    public const string Version = "0.1.7";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
--- Localization/BetterSaveSlotsLoc.cs
+++ Localization/BetterSaveSlotsLoc.cs
@@ -1,30 +1,45 @@
+using JmcModLib.Utils;
 using MegaCrit.Sts2.Core.Localization;
 
 namespace BetterSaveSlots.Localization;
 
 internal static class BetterSaveSlotsLoc
 {
-    public const string Table = "settings_ui";
+    public const string Table = L10n.DefaultTable;
     private const string Prefix = "EXTENSION.BETTERSAVESLOTS.";
 
     public static LocString Loc(string key)
     {
-        return new LocString(Table, Prefix + key);
+        return L10n.Create(Table, BuildKey(key));
     }
 
     public static string Text(string key)
     {
-        return Loc(key).GetFormattedText();
+        string fullKey = BuildKey(key);
+        return L10n.Resolve(fullKey, fullKey, Table);
     }
 
     public static string Format(string key, params (string Name, object Value)[] variables)
     {
-        LocString locString = Loc(key);
-        foreach ((string name, object value) in variables)
-        {
-            locString.AddObj(name, value);
-        }
+        string fullKey = BuildKey(key);
 
-        return locString.GetFormattedText();
+        return L10n.Resolve(
+            fullKey,
+            fullKey,
+            Table,
+            configure: locString =>
+            {
+                foreach ((string name, object value) in variables)
+                {
+                    locString.AddObj(name, value);
+                }
+            });
+    }
+
+    private static string BuildKey(string key)
+    {
+        return key.StartsWith("EXTENSION.", StringComparison.Ordinal)
+            ? key
+            : Prefix + key;
     }
 }
