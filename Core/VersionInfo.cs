0ab97ed737a24040fabf28e11be97520d1ccd50e
MC J <jmc8@qq.com>
2026-05-27 14:20:34 +0800

    发布0.1.4界面与云同步修复

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace BetterSaveSlots.Core;
 public static class VersionInfo
 {
     public const string Name = "BetterSaveSlots";
-    public const string Version = "0.1.3";
+    public const string Version = "0.1.4";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
