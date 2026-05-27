4b42080a1672f48f76a691798105e9312fbbd534
MC J <jmc8@qq.com>
2026-05-27 12:19:49 +0800

    发布0.1.2修复构建

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace BetterSaveSlots.Core;
 public static class VersionInfo
 {
     public const string Name = "BetterSaveSlots";
-    public const string Version = "0.1.1";
+    public const string Version = "0.1.2";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
