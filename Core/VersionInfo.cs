94df57053fc395058c135c4e0903976c2677f5ce
MC J <jmc8@qq.com>
2026-05-27 16:45:50 +0800

    发布0.1.5按钮对齐修复

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace BetterSaveSlots.Core;
 public static class VersionInfo
 {
     public const string Name = "BetterSaveSlots";
-    public const string Version = "0.1.4";
+    public const string Version = "0.1.5";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
