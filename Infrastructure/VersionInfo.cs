b596ec1a09e74dc8a1c423061be3d79680e5efd0
MC J <jmc8@qq.com>
2026-05-28 22:03:46 +0800

    更新0.1.11存档管理头像

--- Infrastructure/VersionInfo.cs
+++ Infrastructure/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace BetterSaveSlots.Infrastructure;
 public static class VersionInfo
 {
     public const string Name = "BetterSaveSlots";
-    public const string Version = "0.1.10";
+    public const string Version = "0.1.11";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
